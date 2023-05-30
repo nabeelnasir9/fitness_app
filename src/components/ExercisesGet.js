a6a4b2e6f05eaa76e330c215c7d1a69921076e8a
nabeelnasir9 <nasirnabeel36@gmail.com>
2023-05-30 15:21:01 +0500

    update 2.0

--- src/components/ExercisesGet.js
+++ src/components/ExercisesGet.js
@@ -7,6 +7,17 @@ import ExerciseCard from './ExerciseCard';
 
 
 const ExercisesGet = ({setExercises, bodyPart, exercises}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const  exercisePerPage = 9;
+  const paginate = (e, value) =>{
+    setCurrentPage(value);
+    window.scrollTo({top:1800, behavior:'smooth'})
+  }
+  const indexOfLastexercise = currentPage * exercisePerPage;
+  const indexOfFirstexercise = indexOfLastexercise - exercisePerPage;
+  const currentExercise= exercises.slice(indexOfFirstexercise, indexOfLastexercise);
+
+
   return (
     <Box id="exercises"
     sx={{
@@ -17,10 +28,24 @@ Showing Results
       </Typography>
       <Stack direction="row" sx={{gap:{lg:'110px', xs:'50px'}}} flexWrap="wrap" justifyContent="center">
      
-     {exercises.map((exercise, index)=>(
+     {currentExercise.map((exercise, index)=>(
       <ExerciseCard key={index} exercise={exercise}/>
      ))}
       </Stack>
+      <Stack mt="100px" alignItems="center">
+{exercises.length > 9 && (
+  <Pagination 
+  color="standard"
+  shape='rounded'
+  defaultPage={1}
+  count={Math.ceil(exercises.length/exercisePerPage)}
+  page={currentPage}
+  onChange={paginate}
+  size="large"
+  />
+)}
+      </Stack>
+
     </Box>
   )
 }
