eb0bb8f656037a5e1a7572c566aa9ac4a3ad09c4
nabeelnasir9 <nasirnabeel36@gmail.com>
2023-05-30 12:47:26 +0500

    update 2.0

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -18,8 +18,16 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+
+    onClick = {()=>{
+        setBodyPart(item);
+        window.scrollTo({top:1800, left:100, behavior:'smooth'})
+    }}
     >
       <img src={Icon} alt="dumbell" style={{ width: "40px", height: "40px" }} />
+      <Typography fontSize="24px" fontWeight="bold" color="#3A1212" textTransform="capitalize" justifyContent="center">
+        {item}
+      </Typography>
     </Stack>
   );
 };
--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -1,11 +1,35 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box , Typography} from '@mui/material'
+import React, {useContext} from 'react'
 import BodyPart from './BodyPart'
 import { ScrollMenu, VisibilityContext  } from 'react-horizontal-scrolling-menu'
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+
+
+
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollPrev()} className="right-arrow">
+        <img src={LeftArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
+  
+  const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollNext()} className="left-arrow">
+        <img src={RightArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
 
 const HorizontalScrollBar = ({data, bodyPart, setBodyPart}) => {
   return (
-    <ScrollMenu>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item)=>(
         <Box 
         key={item.id || item}
